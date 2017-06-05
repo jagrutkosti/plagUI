02252e777c4af541c72cc2c8adacae56be960067
Jagrut Kosti <jagrut.kosti@uni-konstanz.de>
2017-06-05 16:00:49 +0900

    Initial Commit

---
+++ src/main/java/com/plagui/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.plagui.config.audit;
---
+++ src/main/java/com/plagui/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.plagui.repository;
+
+import com.plagui.domain.Authority;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Authority entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
+}
